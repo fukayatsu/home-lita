c91caf113a89821171048be945f3e31fc48f1269
fukayatsu <fukayatsu@gmail.com>
2013-08-04 18:43:05 +0900

    センサーを一つずつ読み取る

--- schedules/sensors.rb
+++ schedules/sensors.rb
@@ -9,8 +9,11 @@ module Lita
       end
 
       def report_status
-        light_str, temp_str, humidity_str = fetch_status
-        status = "temperature:#{temp_str}, humidity:#{humidity_str}, light:#{light_str}"
+        temperature_str = fetch_temperature
+        lights_str      = fetch_lights
+        humidity_str    = fetch_humidity
+
+        status = "[気温]#{temperature_str}℃   [湿度]#{humidity_str}  [明るさ]#{lights_str}"
 
         room   = Lita.config.schedules.lights.room
         target = Struct.new(:room).new(room)
@@ -19,40 +22,46 @@ module Lita
 
       private
 
-      def fetch_status
+      def fetch_temperature
         board = Dino::Board.new(Dino::TxRx.new)
-        light_sensor       = Dino::Components::Sensor.new(pin: 'A0', board: board)
         temperature_sensor = Dino::Components::Sensor.new(pin: 'A2', board: board)
-        humidity_sensor    = Dino::Components::Sensor.new(pin: 'A3', board: board)
-
-        light_dataset = []
-        light_sensor.when_data_received do |data|
-          light_dataset << data.to_i
-        end
 
         temp_dataset = []
         temperature_sensor.when_data_received do |data|
           temp_dataset << data.to_i
         end
+        sleep 1
+
+        temp_value = temp_dataset.inject(:+).to_f / temp_dataset.size
+        temp_str   = "%2.1f" % (temp_value / 1024 * 5 / 0.01)
+      end
+
+      def fetch_lights
+        board = Dino::Board.new(Dino::TxRx.new)
+        light_sensor       = Dino::Components::Sensor.new(pin: 'A0', board: board)
+
+        light_dataset = []
+        light_sensor.when_data_received do |data|
+          light_dataset << data.to_i
+        end
+        sleep 1
+
+        light_value = light_dataset.inject(:+).to_f    / light_dataset.size
+        light_str   = "%1.1f" % Math.log(light_value + 1.0)
+      end
+
+      def fetch_humidity
+        board = Dino::Board.new(Dino::TxRx.new)
+        humidity_sensor    = Dino::Components::Sensor.new(pin: 'A3', board: board)
 
         humidity_dataset = []
         humidity_sensor.when_data_received do |data|
           humidity_dataset << data.to_i
         end
+        sleep 1
 
-        sleep 2
-
-        light_value     = light_dataset.inject(:+).to_f / light_dataset.size
-        temp_value     = temp_dataset.inject(:+).to_f / temp_dataset.size
         humidity_value  = humidity_dataset.inject(:+).to_f / humidity_dataset.size
-        p humidity_dataset.inject(:+).to_f
-        p humidity_dataset.size
-
-        light_str = "%1.1f" % Math.log(light_value + 1.0)
-        temp_str  = "%2.1f" % (temp_value / 1024 * 5 / 0.01)
         humidity_str  = "%2.1f" % (humidity_value / 1024 * 5 / 0.01)
-
-        [light_str, temp_str, humidity_str]
       end
     end
 
