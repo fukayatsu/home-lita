722c8cad596bcd4471133623421e05e02f9270a6
fukayatsu <fukayatsu@gmail.com>
2013-12-01 00:50:36 +0900

    Add chrome app

---
+++ chrome_app/src/js/window.js
@@ -0,0 +1,53 @@
+(function(){
+  function valueChanged(settings){
+    $('#room_id').val(settings.room_id);
+    $('#token').val(settings.token);
+    console.log('value changed.');
+  }
+
+  $(document).on('click', '#save_settings', function(event) {
+    var settings = {
+      "room_id": $('#room_id').val(),
+      "token":   $('#token').val()
+    }
+    chrome.storage.sync.set({settings: JSON.stringify(settings)}, function() {
+      console.log('setting saved.');
+    });
+    event.preventDefault();
+  });
+
+  $(document).on('click', '[data-remote]', function(event) {
+    var message = {
+      room_id: $('#room_id').val(),
+      from:    'ChromeApp',
+      message: 'lita remote ' + $(this).data('remote')
+    };
+    var url = 'https://api.hipchat.com/v1/rooms/message?format=json&auth_token=' + $('#token').val();
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: message,
+      success: function(res) {
+        console.log(res);
+      },
+      error: function (res) {
+        console.log(res);
+      }
+    });
+
+  });
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    if (changes["settings"]) {
+      valueChanged(JSON.parse(changes["settings"].newValue));
+      console.log('chrome.storage.onChanged');
+    }
+  });
+  chrome.storage.sync.get("settings", function(json) {
+    valueChanged(JSON.parse(json));
+    console.log('chrome.storage.sync.get')
+  });
+  console.log('start')
+
+})();
